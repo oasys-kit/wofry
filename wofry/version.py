c0384983359b78a19894440f7320d6142ba2976c
lucarebuffi <lrebuffi@anl.gov>
2019-08-07 10:17:08 -0500

    Debug

---
+++ wofry/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofry SETUP.PY
+short_version = '1.0.20'
+version = '1.0.20'
+full_version = '1.0.20'
+git_revision = '10b868ff00ee1b70ccc4b5f11313b6eaabb72424'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
