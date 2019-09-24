e0effc599ae026821f7e39988a82a607d0413292
lucarebuffi <lrebuffi@anl.gov>
2019-09-24 10:42:15 -0500

    New Features

--- wofry/version.py
+++ wofry/version.py
@@ -3,7 +3,7 @@
 short_version = '1.0.22'
 version = '1.0.22'
 full_version = '1.0.22'
-git_revision = 'a6c094e7216f832809498f86362b81212c8bdf58'
+git_revision = '29ed5553c11b27097c00f3bdf3ec580a5ee4a9fd'
 release = True
 
 if not release:
