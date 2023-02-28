ed40f7b644ec57f239f1574574f31846e829a54e
lucarebuffi <lrebuffi@anl.gov>
2023-02-28 15:03:46 -0600

    syned upgrade

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except AttributeError:
 
 NAME = 'wofry'
 
-VERSION = '1.0.30'
+VERSION = '1.0.31'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon) kernel library'
@@ -49,7 +49,7 @@ CLASSIFIERS = (
 )
 
 INSTALL_REQUIRES = (
-    'syned>=1.0.18',
+    'syned>=1.0.26',
     'h5py',
     'srxraylib'
 )
